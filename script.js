f65e5ec8d54fb472f8acb90b71f221998542da15
IsabelliPinho <108484363+IsabelliPinho@users.noreply.github.com>
2022-12-09 22:05:59 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,125 @@
+let input = document.getElementById("botao")
+let select = document.getElementById("moedas")
+let primeiro = document.getElementById("seleciona") 
+const url = 'https://economia.awesomeapi.com.br/last/'
+const coin = 'USD-BRL,EUR-BRL'
+
+
+async function converterMoedas(){
+
+    let moedas = await fetch(url + coin).then(function(resposta){
+        return resposta.json()
+    })
+
+    
+    let euro = moedas.EURBRL.high
+    let dolar = moedas.USDBRL.high
+
+    let inputdoReal = Number(document.getElementById("input").value)
+    let inputMoedas = document.getElementById("inputM")
+    let textoReal = document.getElementById("texto-real")
+
+    if (primeiro.value === "real") {
+        
+        if (select.value === "dolar") {
+            textoReal.innerHTML = inputdoReal
+            let valorEmDolares = inputdoReal / dolar
+            inputMoedas.innerHTML=valorEmDolares.toLocaleString("en-US", {style: "currency", currency: "EUA"})
+
+        } 
+        if (select.value === "euro"){
+            let valorEmEuros = inputdoReal / euro
+            inputMoedas.innerHTML=valorEmEuros.toLocaleString("de-DE", {style: "currency", currency: "EUR"})
+        }
+        if (select.value === "real") {
+            inputMoedas.innerHTML=inputdoReal.toLocaleString("pt-br", {style: "currency", currency: "BRL"})
+            
+        }
+        
+    }
+    if (primeiro.value === "dolar"){
+        
+        let dolarvezesReal = dolar * inputdoReal
+
+        if (select.value === "real") {
+            inputMoedas.innerHTML = dolarvezesReal.toLocaleString("pt-br", {style: "currency", currency: "BRL"})
+        }
+        if (select.value === "euro") {
+            let dolarToEuro = dolarvezesReal / euro
+            inputMoedas.innerHTML = dolarToEuro.toLocaleString("de-DE", {style: "currency", currency: "EUR"})
+        }
+        if (select.value === "dolar") {
+            inputMoedas.innerHTML = inputdoReal.toLocaleString("en-US", {style: "currency", currency: "USD"})
+        }
+    }
+    if (primeiro.value === "euro") {
+        
+        let eurovezesReal = euro * inputdoReal
+
+        if (select.value === "real") {
+            inputMoedas.innerHTML = eurovezesReal.toLocaleString("pt-br", {style: "currency", currency: "BRL"})
+            
+        }
+        if (select.value === "dolar") {
+            let euroToDolar = eurovezesReal / dolar
+            inputMoedas.innerHTML = euroToDolar.toLocaleString("en-US", {style: "currency", currency: "USD"})  
+        }
+        if (select.value === "euro") {
+            inputMoedas.innerHTML = inputdoReal.toLocaleString("de-DE", {style: "currency", currency: "EUR"}) 
+        }
+        
+    }
+
+
+
+
+
+    textoReal.innerHTML= inputdoReal.toLocaleString("pt-br",{style: "currency", currency: "BRL"})
+}
+
+function bandeira1() {       
+    let textoMoedas1 = document.getElementById("texto-moedas1")
+    let bandeiraMoedas1 = document.getElementById("bandeira-moedas1")
+
+
+    if (primeiro.value === "real") {
+        textoMoedas1.innerHTML ="Real Brasileiro"
+        bandeiraMoedas1.src ="imagem/brasil.png"
+    }
+
+    if (primeiro.value === "euro") {
+        textoMoedas1.innerHTML ="Euro"
+        bandeiraMoedas1.src ="imagem/euro.png"
+    }
+
+    if (primeiro.value === "dolar") {
+        textoMoedas1.innerHTML ="Dolar Americano"
+        bandeiraMoedas1.src ="imagem/eua.png"
+    }
+    converterMoedas()    
+}
+
+function trocaDeMoeda(){
+    let textoMoedas = document.getElementById("texto-moedas")
+    let bandeiraMoedas = document.getElementById("bandeira-moedas")
+
+
+    if(select.value === "dolar"){
+        textoMoedas.innerHTML ="Dolar Americano"
+        bandeiraMoedas.src ="imagem/eua.png"
+    }
+    if(select.value ==="euro"){
+        textoMoedas.innerHTML ="Euro"
+        bandeiraMoedas.src ="imagem/euro.png"
+    }
+    if(select.value === "real"){
+        textoMoedas.innerHTML ="Real Brasileiro"
+        bandeiraMoedas.src ="imagem/brasil.png"
+    }
+
+    converterMoedas()
+}
+
+primeiro.addEventListener("change", bandeira1)
+botao.addEventListener("click", converterMoedas)
+select.addEventListener("change", trocaDeMoeda)
